6d143dd1f2ab0f13b5373dade6090d11a3d9bacc
salamun-cbs <salamun.hmp@gmail.com>
2023-09-07 07:25:09 +0700

    singkatkan variable

--- dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
@@ -3,22 +3,22 @@ Program perulangan membaca buku dengan while sampai paham
 """
 jumlah_buku = 10
 print('Ibu berkata, "baca semua bukumu"')
-total_jumlah_baca = 0
+jumlah_baca = 0
 
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+jumlah_paham = 0
+print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}')
 
-while total_jumlah_baca < jumlah_buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1 } belum pahami")
+while jumlah_baca < jumlah_buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 9:
+        print(f"buku ke {jumlah_paham + 1 } belum pahami")
     else:
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+        jumlah_paham = jumlah_paham + 1
+        print(f"buku ke {jumlah_paham} sudah dibaca dan dipahami")
 
-print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku')
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_paham} buku')
+if jumlah_paham == jumlah_buku:
     print('Bu, semua buku sudah dibaca dan dipahami')
 else:
     print(f'Bu, tidak semua buku bisa dipahami. '
-          f'Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku ')
+          f'Budi hanya bisa memahami {jumlah_paham} buku ')
