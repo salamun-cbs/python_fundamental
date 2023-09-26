3c0de8ba2170dc03b6faa166eb81c6802bb497b6
salamun-cbs <salamun.hmp@gmail.com>
2023-09-26 10:10:05 +0700

    finish fundamental python

---
+++ dasar_sintaksis_dict.py
@@ -0,0 +1,37 @@
+users = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+         "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
+}
+
+print(users)
+print(users["id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"]["street"])
+print(users["address"]["geo"])
+print(users["address"]["geo"]["lat"])
+print(users["address"]["geo"]["lng"])
+
+print(users)
+print(type(users))
+print("\nUbah dict to json")
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open("result.json", "w") as file:
+    json.dump(users, file)
